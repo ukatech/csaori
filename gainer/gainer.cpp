0d2c71f5aab114a5bbcaa83c6de4381a7f7cee06
ponapalt <ponapalt@2c982c0d-f248-0410-a771-998648ebd3bd>
2009-07-07 23:24:15 +0000

    Gainer SAORI initial

---
+++ gainer/gainer.cpp
@@ -0,0 +1,252 @@
+#ifdef _MSC_VER
+#pragma warning( disable : 4786 )
+#endif
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include "csaori.h"
+#include "gainercxx.h"
+
+static std::vector<Gainer*> g_gainer;
+static HWND g_hwnd;
+
+/*---------------------------------------------------------
+	初期化
+---------------------------------------------------------*/
+bool CSAORI::load()
+{
+	return true;
+}
+
+/*---------------------------------------------------------
+	解放
+---------------------------------------------------------*/
+bool CSAORI::unload()
+{
+	size_t n = g_gainer.size();
+	for ( size_t i = 0 ; i < n ; ++i ) {
+		g_gainer[i]->Exit();
+		delete g_gainer[i];
+	}
+	g_gainer.clear();
+	return true;
+}
+
+/*---------------------------------------------------------
+	実行
+---------------------------------------------------------*/
+void CSAORI::exec(const CSAORIInput& in,CSAORIOutput& out)
+{
+	out.result_code = SAORIRESULT_BAD_REQUEST;
+
+	//******************** パラメータ1 = サブコマンド ********************
+	if ( ! in.args.size() ) {
+		return;
+	}
+	string_t subcommand = in.args[0];
+
+	if ( wcsicmp(subcommand.c_str(),L"search") == 0 ) {
+		std::vector<int> port_array;
+		Gainer::Search(port_array);
+
+		char_t buf[32];
+
+		out.result_code = SAORIRESULT_OK;
+		swprintf(buf,L"%u",port_array.size());
+		out.result = buf;
+
+		size_t n = port_array.size();
+		for ( size_t i = 0 ; i < n ; ++i ) {
+			swprintf(buf,L"%i",port_array[i]);
+			out.values.push_back(buf);
+		}
+		return;
+	}
+
+	if ( wcsicmp(subcommand.c_str(),L"hwnd") == 0 ) {
+		if ( in.args.size() < 2 ) {
+			return;
+		}
+		out.result_code = SAORIRESULT_OK;
+		out.values.push_back(L"OK");
+		g_hwnd = reinterpret_cast<HWND>(wcstoul(in.args[1].c_str(),NULL,10));
+		return;
+	}
+
+	//******************** パラメータ2 = ポート番号 ********************
+	if ( in.args.size() < 2 ) {
+		return;
+	}
+	int port = _wtoi(in.args[1].c_str());
+	if ( port < 0 || port > 128 ) {
+		return;
+	}
+
+	//Gainer初期化
+	Gainer *pGainer = NULL;
+	if ( port == 0 ) {
+		if ( g_gainer.size() == 0 ) {
+			std::vector<int> port_array;
+			Gainer::Search(port_array);
+
+			if ( port_array.size() == 0 ) {
+				out.result_code = SAORIRESULT_NO_CONTENT;
+				out.result = L"Gainer not found.";
+				return;
+			}
+
+			port = port_array[0];
+		}
+		else {
+			pGainer = g_gainer[0];
+		}
+	}
+
+	if ( ! pGainer ) {
+		size_t n = g_gainer.size();
+		for ( size_t i = 0 ; i < n ; ++i ) {
+			if ( g_gainer[i]->port() == port ) {
+				pGainer = g_gainer[i];
+			}
+		}
+	}
+
+	if ( ! pGainer ) {
+		pGainer = new Gainer;
+		if ( ! pGainer->Init(port) ) {
+			out.result_code = SAORIRESULT_NO_CONTENT;
+			out.result = L"Gainer not found.";
+			return;
+		}
+
+		g_gainer.push_back(pGainer);
+	}
+
+	//以降パラメータ2の場合のコマンド群
+	if ( wcsicmp(subcommand.c_str(),L"in.analog.all") == 0 ) {
+		out.result_code = SAORIRESULT_OK;
+
+		std::vector<BYTE> result;
+		if ( ! pGainer->getAnalogInputAll(result) ) {
+			out.result = L"NG";
+		}
+		else {
+			out.result = L"OK";
+			char_t buf[32];
+			size_t n = result.size();
+			for ( size_t i = 0 ; i < n ; ++i ) {
+				swprintf(buf,L"%d",result[i]);
+				out.values.push_back(buf);
+			}
+		}
+		return;
+	}
+
+	if ( wcsnicmp(subcommand.c_str(),L"in.digital.all",14) == 0 ) {
+		out.result_code = SAORIRESULT_OK;
+
+		WORD result;
+		size_t bits;
+		if ( ! pGainer->getDigitalInputAll(result,bits) ) {
+			out.result = L"NG";
+		}
+		else {
+			out.result = L"OK";
+
+			char_t buf[32];
+			if ( wcsicmp(subcommand.c_str()+14,L".bit") == 0 ) {
+				DWORD r2 = result;
+				for ( size_t i = 0 ; i < bits ; ++i ) {
+					if ( (r2 & (0UL << i)) != 0 ) {
+						out.values.push_back(L"1");
+					}
+					else {
+						out.values.push_back(L"0");
+					}
+				}
+			}
+			else {
+				swprintf(buf,L"%u",result);
+				out.values.push_back(buf);
+			}
+		}
+		return;
+	}
+
+	//******************** パラメータ3 ********************
+	if ( in.args.size() < 3 ) {
+		return;
+	}
+
+	if ( wcsicmp(subcommand.c_str(),L"config") == 0 ) {
+		out.result_code = SAORIRESULT_OK;
+		out.result = pGainer->setConfiguration(_wtoi(in.args[2].c_str())) ? L"OK" : L"NG";
+		return;
+	}
+
+	if ( wcsicmp(subcommand.c_str(),L"out.led") == 0 ) {
+		out.result_code = SAORIRESULT_OK;
+		if ( _wtoi(in.args[2].c_str()) != 0 ) {
+			out.result = pGainer->turnOnLED() ? L"OK" : L"NG";
+		}
+		else {
+			out.result = pGainer->turnOffLED() ? L"OK" : L"NG";
+		}
+		return;
+	}
+
+	if ( wcsnicmp(subcommand.c_str(),L"out.digital.all",15) == 0 ) {
+		out.result_code = SAORIRESULT_OK;
+
+		DWORD outd = 0;
+		if ( wcsicmp(subcommand.c_str()+15,L".bit") == 0 ) {
+			size_t n = in.args.size();
+			for ( size_t i = 2 ; i < n ; ++i ) {
+				outd |= 1UL << (i-2);
+			}
+		}
+		else {
+			outd = _wtoi(in.args[2].c_str());
+		}
+		out.result = pGainer->setDigitalOutputAll(outd) ? L"OK" : L"NG";
+		return;
+	}
+
+	//******************** パラメータ4 ********************
+	if ( in.args.size() < 4 ) {
+		return;
+	}
+
+	if ( wcsicmp(subcommand.c_str(),L"out.analog.single") == 0 ) {
+		out.result_code = SAORIRESULT_OK;
+
+		int port = _wtoi(in.args[2].c_str());
+		int data = _wtoi(in.args[3].c_str());
+
+		out.result = pGainer->setAnalogOutput(port,data) ? L"OK" : L"NG";
+		return;
+	}
+
+	if ( wcsicmp(subcommand.c_str(),L"out.digital.single") == 0 ) {
+		out.result_code = SAORIRESULT_OK;
+		
+		int port = _wtoi(in.args[2].c_str());
+		int data = _wtoi(in.args[3].c_str());
+
+		out.result = pGainer->setDigitalOutput(port,data != 0) ? L"OK" : L"NG";
+		return;
+	}
+
+	if ( wcsicmp(subcommand.c_str(),L"out.servo.single") == 0 ) {
+		out.result_code = SAORIRESULT_OK;
+
+		int port = _wtoi(in.args[2].c_str());
+		int data = _wtoi(in.args[3].c_str());
+
+		out.result = pGainer->setServoOutput(port,data) ? L"OK" : L"NG";
+		return;
+	}
+
+
+}
+
---
+++ gainer/readme.txt
@@ -0,0 +1,134 @@
+----------------------------------------------------------------------
+■「gainer」：現在開いているウインドウを列挙するSAORI
+■Written by CSaori Project / original by 櫛ヶ浜やぎ
+　http://code.google.com/p/csaori/
+----------------------------------------------------------------------
+
+■これは何をするものか
+
+　現在開いているウインドウを列挙するためのSAORIです。
+
+■動作環境
+
+・Win95/NT4以上
+
+■使用方法
+
+[汎用コマンド]
+必ず1パラメータ以上必要です。
+1つめのパラメータ(Argument0)は「コマンド」です。以降の項目名を設定して下さい。
+
+□search
+・機能
+　Gainerが接続されたポート番号を返します。
+
+・結果
+　Result：列挙できたポートの総数 / 0は見つからなかった
+　Value0～：順番にポート番号
+
+□hwnd
+・機能
+　Gainerからの結果をSSTPで通知するためのhwndです（ボタン状態など）。
+　不要な場合は設定する必要はありません。
+
+・パラメータ
+　Argument1：SSTPを受け付けるウインドウのHWND
+
+・結果
+　Result：OK
+
+[Gainer用コマンド群]
+必ず2パラメータ以上必要です。
+1つめのパラメータ(Argument0)は「コマンド」です。以降の項目名を設定して下さい。
+2つめのパラメータ(Argument1)は「シリアルポート番号」です。
+1から128までの数値を指定するか、あるいは0を指定すると勝手にGainerを探索します。
+
+□config
+・機能
+　Gainerのモードを設定します(KONFIGURATION_n命令)
+　
+・パラメータ
+　Argument2：モード番号(1～8をサポート)
+
+・結果
+　Result：OK/NG
+
+□in.analog.all
+・機能
+　アナログ入力ピンの状態をすべて取得します
+
+・結果
+　Result：OK/NG
+　Value0～：
+
+□out.analog.single
+・機能
+　1つのアナログ出力ピンへの出力電圧を変更します。
+
+・パラメータ
+　Argument2：ピン番号
+　Argument3：値(0～255)
+
+・結果
+　Result：OK/NG
+
+□in.digital.all
+□in.digital.all.bit
+・機能
+　デジタル入力ピンの状態をすべて取得します
+
+・結果
+　Result：OK/NG
+　Value0：ピン0を最下位ビットとして解釈した場合の数値
+　Value0～：(.bit)ピン0から順番に0/1
+
+□out.digital.all
+□out.digital.all.bit
+・機能
+　デジタル出力ピンの状態をすべて変更します
+
+・パラメータ
+　Argument2：ピン0を最下位ビットとして解釈した場合の数値
+　Argument2～：(.bit)ピン0から順番に0/1
+
+□out.digital.single
+・機能
+　1つのデジタル出力ピンへの出力フラグを変更します。
+
+・パラメータ
+　Argument2：ピン番号
+　Argument3：0/1
+
+・結果
+　Result：OK/NG
+
+□out.servo.single
+・機能
+　1つのRCサーボ制御ピンへの出力情報を変更します。
+　[注意] Gainer mini config=8 のみのサポートです。
+
+・パラメータ
+　Argument2：ピン番号
+　Argument3：0～255
+
+・結果
+　Result：OK/NG
+
+□out.led
+・機能
+　Gainer基盤上のLEDを光らせます
+
+・パラメータ
+　Argument2：1で点灯、0で消灯
+
+・結果
+　Result：OK/NG
+
+■配布条件等
+
+license.txtを見てください。
+
+■更新履歴
+
+・2008/8/16 Initial Release
+
