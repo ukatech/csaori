b91201da01b988e6717a5fab0dfd18b73076c373
ponapalt <ponapalt@2c982c0d-f248-0410-a771-998648ebd3bd>
2009-07-16 14:08:51 +0000

    gainer 1.1.2   SecurityLevelヘッダの解釈がうまくいかない問題を修正 　readのタイミング調整

--- gainer/gainer.cpp
+++ gainer/gainer.cpp
@@ -7,6 +7,16 @@
 #include "csaori.h"
 #include "gainercxx.h"
 
+//////////WINDOWS DEFINE///////////////////////////
+//includeÇÃÇ†Ç∆Ç…Ç®Ç¢ÇƒÇÀÅI
+#ifdef _WINDOWS
+#ifdef _DEBUG
+#include <crtdbg.h>
+#define new new( _NORMAL_BLOCK, __FILE__, __LINE__)
+#endif
+#endif
+///////////////////////////////////////////////////
+
 static std::vector<CGainer*> g_gainer;
 static HWND g_hwnd;
 
