2990eefca46a3f0a8d164773c7f05873477213a6
roytam <roytam@2c982c0d-f248-0410-a771-998648ebd3bd>
2009-07-24 14:43:35 +0000

    CSAORIでhttpc (未完成)

---
+++ chttpc/CSAORIDSSTP.h
@@ -0,0 +1,12 @@
+class CSAORIDSSTP{
+public:
+	unsigned int codepage;
+	HWND hwnd;
+	string_t sender;
+	string_t event;
+	std::vector<string_t> refs;
+	std::map<string_t,string_t> opts;
+
+	string_t toString();
+};
+
