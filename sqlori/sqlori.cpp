1b20321ff649a3dcbce565a689d75a83d6027d64
twinsec <twinsec@2c982c0d-f248-0410-a771-998648ebd3bd>
2009-07-25 14:49:20 +0000

    put back SQLite unrelated classes to csaori_interface

--- sqlori/sqlori.cpp
+++ sqlori/sqlori.cpp
@@ -1,8 +1,8 @@
-#include "csaori.h"
+﻿#include "csaori.h"
 #include "sqlite3.h"
 #include "sqlori.h"
 
-int Sqlori::open(const CSAORIInput& in, const string_t &path)
+int Sqlori::open(const std::string &path)
 {
 	int res = 0;
 	char *errMsg = NULL;
@@ -10,15 +10,7 @@ int Sqlori::open(const CSAORIInput& in, const string_t &path)
 	if (close())
 		return SQLITE_ERROR;
 
-	std::string file;
-
-	if (in.args.size() == 1) {
-		file = SAORI_FUNC::UnicodeToMultiByte(path + L"sqlori.db", CP_UTF8);
-	}
-	else {
-		file = SAORI_FUNC::UnicodeToMultiByte(path + in.args[1], CP_UTF8);
-	}
-	res = sqlite3_open(file.c_str(), &_db);
+	res = sqlite3_open(path.c_str(), &_db);
 
 	if (res != SQLITE_OK) {
 		_result = sqlite3_errmsg(_db);
@@ -30,20 +22,16 @@ int Sqlori::open(const CSAORIInput& in, const string_t &path)
 	return SQLITE_OK;
 }
 
-int Sqlori::exec(const CSAORIInput& in)
+int Sqlori::exec(const std::string &command)
 {
 	int res = 0;
 	char *errMsg = NULL;
 
 	if (_db) {
-		if (in.args.size() >= 2) {
-			std::string command = SAORI_FUNC::UnicodeToMultiByte(in.args[1], CP_UTF8);
-
-			res = sqlite3_exec(_db, command.c_str(), &callBack, this, &errMsg);
-			if (res != SQLITE_OK) {
-				_result = errMsg;
-				sqlite3_free(errMsg);
-			}
+		res = sqlite3_exec(_db, command.c_str(), &callBack, this, &errMsg);
+		if (res != SQLITE_OK) {
+			_result = errMsg;
+			sqlite3_free(errMsg);
 		}
 		return SQLITE_OK;
 	}
@@ -68,6 +56,7 @@ int Sqlori::close()
 int Sqlori::callBack(void *callee, int size, char **values, char **fields)
 {
 	std::string &result = ((Sqlori *)callee)->_result;
+
 	if (result.empty()) {
 		result += fields[0];
 		for (int i = 1; i < size; i++) {
