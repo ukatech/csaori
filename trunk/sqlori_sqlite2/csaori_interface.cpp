7cfd506540df8673d8f40c5002e778eefefb7d87
roytam <roytam@2c982c0d-f248-0410-a771-998648ebd3bd>
2009-08-16 12:57:26 +0000

    - [sqlori_sqlite2] use input codepage.

--- trunk/sqlori_sqlite2/csaori_interface.cpp
+++ trunk/sqlori_sqlite2/csaori_interface.cpp
@@ -27,7 +27,7 @@ void CSAORI::exec(const CSAORIInput& in,CSAORIOutput& out)
 	}
 	else if (in.args[0] == L"EXEC") {
 		if (in.args.size() >= 2) {
-			res = sql->exec(SAORI_FUNC::UnicodeToMultiByte(in.args[1], CP_UTF8));
+			res = sql->exec(SAORI_FUNC::UnicodeToMultiByte(in.args[1], in.codepage));
 		}
 	}
 	else if (in.args[0] == L"CLOSE") {
@@ -35,12 +35,12 @@ void CSAORI::exec(const CSAORIInput& in,CSAORIOutput& out)
 	}
 	else if (in.args[0] == L"SQLITE_VERSION") {
 		out.result_code = SAORIRESULT_OK;
-		out.result = SAORI_FUNC::intToString(SQLITE_OK) + L"\2Version\2" + SAORI_FUNC::MultiByteToUnicode(sqlite_libversion(), CP_UTF8);
+		out.result = SAORI_FUNC::intToString(SQLITE_OK) + L"\2Version\2" + SAORI_FUNC::MultiByteToUnicode(sqlite_libversion(), in.codepage);
 		return;
 	}
 
 	out.result_code = SAORIRESULT_OK;
-	out.result = SAORI_FUNC::intToString(res) + L"\2" + SAORI_FUNC::MultiByteToUnicode(sql->result(), CP_UTF8);
+	out.result = SAORI_FUNC::intToString(res) + L"\2" + SAORI_FUNC::MultiByteToUnicode(sql->result(), in.codepage);
 	
 //	out.result = L"Charset = " + SAORI_FUNC::CHARSETtoString(in.charset);
 //	out.charset = CHARSET_UTF_8;
