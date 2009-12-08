872d952b0062b7a53f692cdc912d098287f76f2f
ponapalt <ponapalt@2c982c0d-f248-0410-a771-998648ebd3bd>
2009-12-08 23:20:22 +0000

    イベント通知実装

--- tsvnexec/tsvnexec.cpp
+++ tsvnexec/tsvnexec.cpp
@@ -4,18 +4,34 @@
 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
-#include <wininet.h>
+#include <process.h>
 #include <atlbase.h>
-#include <shlobj.h>
 #include <comdef.h>
 #include "csaori.h"
 
 #import "SubWCRevCOM.exe" named_guids
 
+//data struct
+class ExecuteTortoiseProcData {
+public:
+	std::string tsvn;
+	std::string arg;
+	std::wstring path;
+	std::string notify_event;
+	void *hwnd;
+};
+
+//Prototypes
 static bool GetTortoiseProcPath(std::string &path);
+static DWORD ExecuteTortoiseProc(const ExecuteTortoiseProcData &d);
+
+static void _cdecl ExecuteTortoiseProcThread(void *d);
+
+void *g_hwnd = NULL;
 
 bool CSAORI::load()
 {
+	//CoInitialize(NULL);
 	return true;
 }
 
@@ -26,25 +42,31 @@ bool CSAORI::unload()
 
 void CSAORI::exec(const CSAORIInput &in, CSAORIOutput &out)
 {
-	//パラメータ必須 Arg0:パス Arg1:コマンド Arg2以降:サブコマンド
+	//パラメータ必須 Arg0:コマンド Arg1:パス Arg2以降:サブコマンド
 	if ( in.args.size() < 2 ) {
 		out.result_code = SAORIRESULT_BAD_REQUEST;
 		return;
 	}
 
-	const std::wstring &path = in.args[0];
-	const std::wstring &cmd = in.args[1];
+	const std::wstring &cmd = in.args[0];
+	const std::wstring &path = in.args[1];
 
 	out.result_code = SAORIRESULT_OK;
 
-	if ( wcsnicmp(cmd.c_str(),L"info",4) == 0 ) {
+	//***** hwndコマンド *****
+	if ( wcsnicmp(cmd.c_str(),L"hwnd",4) == 0 ) {
+		//このコマンドのみ特殊：Arg1はhwnd
+		g_hwnd = reinterpret_cast<void*>(wcstoul(path.c_str(),NULL,10));
+	}
+	//***** infoコマンド -> SubWCRev/COMを呼び出し *****
+	else if ( wcsnicmp(cmd.c_str(),L"info",4) == 0 ) {
 		if ( in.args.size() < 3 ) {
 			out.result_code = SAORIRESULT_BAD_REQUEST;
 			return;
 		}
 
 		CComPtr<LibSubWCRev::ISubWCRev> pRev;
-		if ( FAILED(pRev.CoCreateInstance(LibSubWCRev::CLSID_SubWCRev,NULL,CLSCTX_ALL)) || pRev == NULL ) {
+		if ( FAILED(pRev.CoCreateInstance(LibSubWCRev::CLSID_SubWCRev,NULL,CLSCTX_LOCAL_SERVER )) || pRev == NULL ) {
 			out.result = L"!ERROR!|TSVN_NOT_FOUND|ISubWCRev";
 			return;
 		}
@@ -104,6 +126,7 @@ void CSAORI::exec(const CSAORIInput &in, CSAORIOutput &out)
 			}
 		}
 	}
+	//***** info以外 *****
 	else {
 		std::string tsvn;
 		if ( ! GetTortoiseProcPath(tsvn) ) {
@@ -111,12 +134,17 @@ void CSAORI::exec(const CSAORIInput &in, CSAORIOutput &out)
 			return;
 		}
 
-		std::string path_a = SAORI_FUNC::UnicodeToMultiByte(path);
-		if ( ::GetFileAttributes(path_a.c_str()) == INVALID_FILE_ATTRIBUTES ) {
-			out.result = L"!ERROR!|PATH_INVALID|TortoiseProc";
-			return;
+		//指定パス存在チェック
+		std::string path_a;
+		if ( path.find(L"*") == std::wstring::npos ) {
+			path_a = SAORI_FUNC::UnicodeToMultiByte(path);
+			if ( ::GetFileAttributes(path_a.c_str()) == INVALID_FILE_ATTRIBUTES ) {
+				out.result = L"!ERROR!|PATH_INVALID|TortoiseProc";
+				return;
+			}
 		}
 
+		//スイッチ構築
 		std::string arg;
 		arg += "/command:" + SAORI_FUNC::UnicodeToMultiByte(cmd) + " ";
 
@@ -125,16 +153,30 @@ void CSAORI::exec(const CSAORIInput &in, CSAORIOutput &out)
 		size_t n = in.args.size();
 
 		bool close_on_end_found = false;
-		bool wait_found = false;
+		void *hwnd = NULL;
+		bool notify_found = false;
+		std::string notify_event;
 
 		for ( size_t i = 2 ; i < n ; ++i ) {
 			const std::wstring &c = in.args[i];
 
-			if ( wcsnicmp(c.c_str(),L"wait",4) || wcsnicmp(c.c_str(),L"/wait",5) ) {
-				wait_found = true;
+			if ( wcsnicmp(c.c_str(),L"notify",6) || wcsnicmp(c.c_str(),L"/notify",7) ) {
+				const wchar_t *pParam;
+				if ( c[0] != L'/' ) {
+					pParam = c.c_str() + 6;
+				}
+				else {
+					pParam = c.c_str() + 7;
+				}
+				notify_found = true;
+
+				if ( pParam[0] == L':' ) {
+					pParam += 1;
+					notify_event = SAORI_FUNC::UnicodeToMultiByte(pParam);
+				}
 			}
 			else {
-				if ( c[0] != L'/' ) {
+				if ( c[0] != L'/' ) { //スラッシュ省略されたときには補う
 					arg += "/";
 				}
 				SAORI_FUNC::UnicodeToMultiByte(c) + " ";
@@ -149,44 +191,138 @@ void CSAORI::exec(const CSAORIInput &in, CSAORIOutput &out)
 			arg += "/closeonend:1 ";
 		}
 
-		PROCESS_INFORMATION pi;
-		STARTUPINFO si;
+		if ( g_hwnd && notify_found ) {
+			ExecuteTortoiseProcData *pData = new ExecuteTortoiseProcData;
+			pData->arg = arg;
+			pData->hwnd = g_hwnd;
+			pData->tsvn = tsvn;
+			pData->path = path;
+			pData->notify_event = notify_event;
 
-		ZeroMemory(&si,sizeof(si));
-		si.cb=sizeof(si);
-
-		const char *pAppName = (const char*)_mbsrchr((const unsigned char*)tsvn.c_str(),'\\');
-		std::string param;
-		if ( pAppName ) {
-			param = pAppName + 1;
+			_beginthread(ExecuteTortoiseProcThread,0,pData);
 		}
 		else {
-			param = tsvn;
-		}
-		param += " ";
-		param += arg;
+			ExecuteTortoiseProcData data;
+			data.arg = arg;
+			data.hwnd = NULL;
+			data.tsvn = tsvn;
+			data.path = path;
+			data.notify_event = notify_event;
 
-		char *paramptr = (char*)malloc(param.size()+1);
-		memcpy(paramptr,param.c_str(),param.size()+1);
-
-		if ( ::CreateProcess(tsvn.c_str(),paramptr,NULL,NULL,FALSE,
-			::GetPriorityClass(::GetCurrentProcess()),
-			NULL,NULL,&si,&pi) ) {
-
-			::CloseHandle(pi.hThread);
-			if ( wait_found ) {
-				::WaitForSingleObject(pi.hProcess,INFINITE);
+			if ( ExecuteTortoiseProc(data) == 0 ) {
+				out.result = L"OK";
 			}
-			::CloseHandle(pi.hProcess);
-
-			out.result = L"OK";
+			else {
+				out.result = L"!ERROR!|TSVN_EXEC_FAIL|TortoiseProc";
+			}
 		}
-		else {
-			out.result = L"!ERROR!|TSVN_EXEC_FAIL|TortoiseProc";
-		}
-
-		free(paramptr);
 	}
+}
+
+/*-----------------------------------------------------
+	スレッドによるTortoiseProc実行部
+------------------------------------------------------*/
+void _cdecl ExecuteTortoiseProcThread(void *ptr)
+{
+	::CoInitialize(NULL);
+
+	ExecuteTortoiseProcData *pData = reinterpret_cast<ExecuteTortoiseProcData*>(ptr);
+	const ExecuteTortoiseProcData &d = *pData;
+
+	CComPtr<LibSubWCRev::ISubWCRev> pRev;
+	if ( FAILED(pRev.CoCreateInstance(LibSubWCRev::CLSID_SubWCRev,NULL,CLSCTX_LOCAL_SERVER )) || pRev == NULL ) {
+		pRev = NULL;
+	}
+
+	long before = -1;
+	if ( pRev && ! FAILED(pRev->GetWCInfo(d.path.c_str(),true,true)) ) {
+		before = pRev->Revision;
+	}
+
+	DWORD result = ExecuteTortoiseProc(*pData);
+
+	long after = -1;
+	if ( pRev && ! FAILED(pRev->GetWCInfo(d.path.c_str(),true,true)) ) {
+		after = pRev->Revision;
+	}
+
+	std::string sstp = "NOTIFY SSTP/1.1\r\nCharset: UTF-8\r\nSender: tsvnexec SAORI\r\n";
+	sstp += "HWnd: 0\r\nEvent: ";
+	if ( strncmp(d.notify_event.c_str(),"On",2) == 0 ) {
+		sstp += d.notify_event;
+	}
+	else {
+		sstp += "OnTSVNExecComplete";
+	}
+	sstp += "\r\n";
+
+	sstp += "Reference0: ";
+	sstp += d.notify_event;
+	sstp += "\r\n";
+
+	char data[128];
+	sprintf(data,"Reference1: %d\r\nReference2: %d\r\nReference3: %d\r\n",static_cast<long>(result),before,after);
+	sstp += data;
+	sstp += "\r\n";
+
+	DWORD sstpresult;
+	
+	COPYDATASTRUCT c;
+	c.dwData = 9801;
+	c.cbData = sstp.size();
+	c.lpData = const_cast<char*>(sstp.c_str());
+
+	SendMessageTimeout(reinterpret_cast<HWND>(d.hwnd),
+		WM_COPYDATA,
+		reinterpret_cast<WPARAM>(d.hwnd),
+		reinterpret_cast<LPARAM>(&c),
+		SMTO_ABORTIFHUNG,1000,&sstpresult);
+
+	delete pData;
+}
+
+/*-----------------------------------------------------
+	TortoiseProc実行部
+------------------------------------------------------*/
+DWORD ExecuteTortoiseProc(const ExecuteTortoiseProcData &d)
+{
+	PROCESS_INFORMATION pi;
+	STARTUPINFO si;
+
+	ZeroMemory(&si,sizeof(si));
+	si.cb=sizeof(si);
+
+	const char *pAppName = (const char*)_mbsrchr((const unsigned char*)d.tsvn.c_str(),'\\');
+	std::string param;
+	if ( pAppName ) {
+		param = pAppName + 1;
+	}
+	else {
+		param = d.tsvn;
+	}
+	param += " ";
+	param += d.arg;
+
+	char *paramptr = (char*)malloc(param.size()+1);
+	memcpy(paramptr,param.c_str(),param.size()+1);
+
+	DWORD result = 0xffffffffU;
+	if ( ::CreateProcess(d.tsvn.c_str(),paramptr,NULL,NULL,FALSE,
+		::GetPriorityClass(::GetCurrentProcess()),
+		NULL,NULL,&si,&pi) ) {
+
+		result = true;
+
+		::CloseHandle(pi.hThread);
+		if ( d.hwnd ) {
+			::WaitForSingleObject(pi.hProcess,INFINITE);
+			::GetExitCodeProcess(pi.hProcess,&result);
+		}
+		::CloseHandle(pi.hProcess);
+	}
+
+	free(paramptr);
+	return result;
 }
 
 /*-----------------------------------------------------
