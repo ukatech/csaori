639d3964c0e53441ed897df7dd3ae6766b2fd980
ukiya.yasagure <ukiya.yasagure@gmail.com>
2010-04-12 08:22:19 +0000

    add ccpuid2 branch To Roy:  please read ccpuid2/README.txt and examine merging. 　this .exe was compiled with VS2005, so can't execute on win95 (maybe)

---
+++ impl_saori/ccpuid2/cpuusage.h
@@ -0,0 +1,66 @@
+#ifndef __CPUUSAGE_H
+#define __CPUUSAGE_H
+#include <windows.h>
+
+#define SystemBasicInformation       0
+#define SystemPerformanceInformation 2
+#define SystemTimeInformation        3
+
+#define Li2Double(x) ((double)((x).HighPart) * 4.294967296E9 + (double)((x).LowPart))
+
+typedef struct
+{
+    DWORD   dwUnknown1;
+    ULONG   uKeMaximumIncrement;
+    ULONG   uPageSize;
+    ULONG   uMmNumberOfPhysicalPages;
+    ULONG   uMmLowestPhysicalPage;
+    ULONG   uMmHighestPhysicalPage;
+    ULONG   uAllocationGranularity;
+    PVOID   pLowestUserAddress;
+    PVOID   pMmHighestUserAddress;
+    ULONG   uKeActiveProcessors;
+    BYTE    bKeNumberProcessors;
+    BYTE    bUnknown2;
+    WORD    wUnknown3;
+} SYSTEM_BASIC_INFORMATION;
+
+typedef struct
+{
+    LARGE_INTEGER   liIdleTime;
+    DWORD           dwSpare[76];
+} SYSTEM_PERFORMANCE_INFORMATION;
+
+typedef struct
+{
+    LARGE_INTEGER liKeBootTime;
+    LARGE_INTEGER liKeSystemTime;
+    LARGE_INTEGER liExpTimeZoneBias;
+    ULONG         uCurrentTimeZoneId;
+    DWORD         dwReserved;
+} SYSTEM_TIME_INFORMATION;
+
+
+// ntdll!NtQuerySystemInformation (NT specific!)
+//
+// The function copies the system information of the
+// specified type into a buffer
+//
+// NTSYSAPI
+// NTSTATUS
+// NTAPI
+// NtQuerySystemInformation(
+//    IN UINT SystemInformationClass,    // information type
+//    OUT PVOID SystemInformation,       // pointer to buffer
+//    IN ULONG SystemInformationLength,  // buffer size in bytes
+//    OUT PULONG ReturnLength OPTIONAL   // pointer to a 32-bit
+//                                       // variable that receives
+//                                       // the number of bytes
+//                                       // written to the buffer
+// );
+typedef LONG (WINAPI *PROCNTQSI)(UINT,PVOID,ULONG,PULONG);
+
+
+UINT CPUUsage();
+
+#endif /* __CPUUSAGE_H */
