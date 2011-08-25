4305d332128b202faef6c344bd099e31fe598549
ponapalt <ponapalt@2c982c0d-f248-0410-a771-998648ebd3bd>
2011-08-25 22:16:48 +0000

    PlayWavSound (PlaySound APIラッパー) 追加

---
+++ trunk/impl_saori/PlayWavSound/PlayWavSound.cpp
@@ -0,0 +1,67 @@
+#ifdef _MSC_VER
+#pragma warning( disable : 4786 )
+#endif
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <mmsystem.h>
+#include "csaori.h"
+
+/*---------------------------------------------------------
+	èâä˙âª
+---------------------------------------------------------*/
+bool CSAORI::load(){
+	return true;
+}
+
+/*---------------------------------------------------------
+	âï˙
+---------------------------------------------------------*/
+bool CSAORI::unload(){
+	return true;
+}
+
+/*---------------------------------------------------------
+	é©ìÆfree
+---------------------------------------------------------*/
+class CAutoFree {
+private:
+	CAutoFree() { }
+
+	void *m_pBuf;
+public:
+	CAutoFree(void* pBuf) : m_pBuf(pBuf) { }
+	~CAutoFree() { if ( m_pBuf ) { free(m_pBuf); } }
+
+	operator void*() { return m_pBuf; }
+};
+
+/*---------------------------------------------------------
+	é¿çs
+---------------------------------------------------------*/
+void CSAORI::exec(const CSAORIInput& in,CSAORIOutput& out){
+	out.result_code = SAORIRESULT_BAD_REQUEST;
+	if ( in.args.size() < 1 ) { return; }
+
+	DWORD flag = SND_ASYNC;
+	std::string filepath;
+
+	if ( wcsnicmp(in.args[0].c_str(),L"system:",7) == 0 ) {
+		flag |= SND_ALIAS_ID;
+		filepath = SAORI_FUNC::UnicodeToMultiByte(in.args[0].c_str()+7);
+	}
+	else {
+		flag |= SND_NODEFAULT | SND_FILENAME;
+		filepath = checkAndModifyPath(SAORI_FUNC::UnicodeToMultiByte(in.args[0]));
+	}
+
+	out.result_code = SAORIRESULT_OK;
+
+	BOOL result = ::PlaySound(filepath.c_str(),NULL,flag);
+
+	char_t buf[32];
+	swprintf(buf,L"%d",result);
+
+	out.result = buf;
+}
+
