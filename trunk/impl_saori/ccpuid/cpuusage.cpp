05e34848853924caa7490a7e06d186688d13a3cb
ponapalt <ponapalt@2c982c0d-f248-0410-a771-998648ebd3bd>
2012-03-13 02:19:43 +0000

    (Temp Commit)

--- trunk/impl_saori/ccpuid/cpuusage.cpp
+++ trunk/impl_saori/ccpuid/cpuusage.cpp
@@ -1,8 +1,67 @@
+#include <pdh.h>
+#include <pdhmsg.h>
+
 #include "cpuusage.h"
 
-UINT CPUUsageNT(void)
+static HMODULE hModulePdh = NULL;
+
+#define SystemBasicInformation       0
+#define SystemPerformanceInformation 2
+#define SystemTimeInformation        3
+
+#define Li2Double(x) ((double)((x).HighPart) * 4.294967296E9 + (double)((x).LowPart))
+
+typedef struct
 {
-    SYSTEM_PERFORMANCE_INFORMATION SysPerfInfo;
+    DWORD   dwUnknown1;
+    ULONG   uKeMaximumIncrement;
+    ULONG   uPageSize;
+    ULONG   uMmNumberOfPhysicalPages;
+    ULONG   uMmLowestPhysicalPage;
+    ULONG   uMmHighestPhysicalPage;
+    ULONG   uAllocationGranularity;
+    PVOID   pLowestUserAddress;
+    PVOID   pMmHighestUserAddress;
+    ULONG   uKeActiveProcessors;
+    BYTE    bKeNumberProcessors;
+    BYTE    bUnknown2;
+    WORD    wUnknown3;
+} SYSTEM_BASIC_INFORMATION;
+
+typedef struct
+{
+    LARGE_INTEGER   liIdleTime;
+    DWORD           dwSpare[76];
+} SYSTEM_PERFORMANCE_INFORMATION;
+
+typedef struct
+{
+    LARGE_INTEGER liKeBootTime;
+    LARGE_INTEGER liKeSystemTime;
+    LARGE_INTEGER liExpTimeZoneBias;
+    ULONG         uCurrentTimeZoneId;
+    DWORD         dwReserved;
+} SYSTEM_TIME_INFORMATION;
+
+typedef LONG (WINAPI *PROCNTQSI)(UINT,PVOID,ULONG,PULONG);
+
+typedef PDH_STATUS (WINAPI* defPdhOpenQueryW)(LPCWSTR, DWORD_PTR, PDH_HQUERY*);
+typedef PDH_STATUS (WINAPI* defPdhAddCounterW)(PDH_HQUERY, LPCWSTR, DWORD_PTR, PDH_HCOUNTER*);
+typedef PDH_STATUS (WINAPI* defPdhCollectQueryData)(PDH_HQUERY);
+typedef PDH_STATUS (WINAPI* defPdhGetFormattedCounterValue)(PDH_HCOUNTER, DWORD, LPDWORD, PPDH_FMT_COUNTERVALUE);
+typedef PDH_STATUS (WINAPI* defPdhCloseQuery)(PDH_HQUERY);
+typedef PDH_STATUS (WINAPI* defPdhRemoveCounter)(PDH_HCOUNTER);
+
+static defPdhOpenQueryW pPdhOpenQueryW = NULL;
+static defPdhAddCounterW pPdhAddCounterW = NULL;
+static defPdhCollectQueryData pPdhCollectQueryData = NULL;
+static defPdhGetFormattedCounterValue pPdhGetFormattedCounterValue = NULL;
+static defPdhCloseQuery pPdhCloseQuery = NULL;
+static defPdhRemoveCounter pPdhRemoveCounter = NULL;
+
+void CPUUsage::CPUUsageThreadNT(void)
+{
+	SYSTEM_PERFORMANCE_INFORMATION SysPerfInfo;
     SYSTEM_TIME_INFORMATION        SysTimeInfo;
     SYSTEM_BASIC_INFORMATION       SysBaseInfo;
     double                         dbIdleTime;
@@ -11,142 +70,162 @@ UINT CPUUsageNT(void)
     LARGE_INTEGER                  liOldIdleTime = {0,0};
     LARGE_INTEGER                  liOldSystemTime = {0,0};
 	PROCNTQSI                      NtQuerySystemInformation;
-
+	
     NtQuerySystemInformation = (PROCNTQSI)GetProcAddress(
-                                          GetModuleHandle("ntdll"),
-                                         "NtQuerySystemInformation"
-                                         );
-
-    if (!NtQuerySystemInformation)
-        return 0;
-
+		GetModuleHandle("ntdll"),
+		"NtQuerySystemInformation"
+		);
+	
+    if (!NtQuerySystemInformation) {
+        m_usage = 0;
+		return;
+	}
+	
     // get number of processors in the system
     status = NtQuerySystemInformation(SystemBasicInformation,&SysBaseInfo,sizeof(SysBaseInfo),NULL);
-    if (status != NO_ERROR)
-        return 0;
+    if (status != NO_ERROR) {
+        m_usage = 0;
+		return;
+	}
     
-    while(1)
-    {
+    while ( ! m_thread_exit ) {
         // get new system time
-    status = NtQuerySystemInformation(SystemTimeInformation,&SysTimeInfo,sizeof(SysTimeInfo),0);
-        if (status!=NO_ERROR)
-            return 0;
-
-        // get new CPU's idle time
-        status = NtQuerySystemInformation(SystemPerformanceInformation,&SysPerfInfo,sizeof(SysPerfInfo),NULL);
-        if (status != NO_ERROR)
-            return 0;
-
-        // if it's a first call - skip it
-       if (liOldIdleTime.QuadPart != 0)
-       {
-            // CurrentValue = NewValue - OldValue
-            dbIdleTime = Li2Double(SysPerfInfo.liIdleTime) - Li2Double(liOldIdleTime);
-            dbSystemTime = Li2Double(SysTimeInfo.liKeSystemTime) - Li2Double(liOldSystemTime);
-
-            // CurrentCpuIdle = IdleTime / SystemTime
-            dbIdleTime = dbIdleTime / dbSystemTime;
-
-            // CurrentCpuUsage% = 100 - (CurrentCpuIdle * 100) / NumberOfProcessors
-            dbIdleTime = 100.0 - dbIdleTime * 100.0 / (double)SysBaseInfo.bKeNumberProcessors + 0.5;
-
-            return (UINT)dbIdleTime;
-       }
-
-        // store new CPU's idle and system time
-        liOldIdleTime = SysPerfInfo.liIdleTime;
-        liOldSystemTime = SysTimeInfo.liKeSystemTime;
-
-        // wait half second
-        Sleep(500);
+		status = NtQuerySystemInformation(SystemTimeInformation,&SysTimeInfo,sizeof(SysTimeInfo),0);
+        if (status!=NO_ERROR) {
+			// get new CPU's idle time
+			status = NtQuerySystemInformation(SystemPerformanceInformation,&SysPerfInfo,sizeof(SysPerfInfo),NULL);
+			if (status != NO_ERROR) {		
+				// if it's a first call - skip it
+				if (liOldIdleTime.QuadPart != 0) {
+					// CurrentValue = NewValue - OldValue
+					dbIdleTime = Li2Double(SysPerfInfo.liIdleTime) - Li2Double(liOldIdleTime);
+					dbSystemTime = Li2Double(SysTimeInfo.liKeSystemTime) - Li2Double(liOldSystemTime);
+					
+					// CurrentCpuIdle = IdleTime / SystemTime
+					dbIdleTime = dbIdleTime / dbSystemTime;
+					
+					// CurrentCpuUsage% = 100 - (CurrentCpuIdle * 100) / NumberOfProcessors
+					dbIdleTime = 100.0 - dbIdleTime * 100.0 / (double)SysBaseInfo.bKeNumberProcessors + 0.5;
+					
+					m_usage = (UINT)dbIdleTime;
+				}
+				
+				// store new CPU's idle and system time
+				liOldIdleTime = SysPerfInfo.liIdleTime;
+				liOldSystemTime = SysTimeInfo.liKeSystemTime;
+			}
+		}
+		
+        // wait second
+        WaitForSingleObject(m_h_event,1000);
     }
 }
 
 
-UINT CPUUsage9X(void)
+void CPUUsage::CPUUsageThread9X(void)
 {
     HKEY hkey;
     DWORD dwDataSize;
     DWORD dwType;
     DWORD dwCpuUsage;
     UINT usage;
-
+	
     // starting the counter
     if ( RegOpenKeyEx( HKEY_DYN_DATA,
-                       "PerfStats\\StartStat",
-                       0,KEY_ALL_ACCESS,
-                       &hkey ) != ERROR_SUCCESS)
-       return 0;
-
+		"PerfStats\\StartStat",
+		0,KEY_ALL_ACCESS,
+		&hkey ) != ERROR_SUCCESS) {
+		m_usage = 0;
+		return;
+	}
+	
     dwDataSize=sizeof(DWORD);
     RegQueryValueEx( hkey,
-                     "KERNEL\\CPUUsage",
-                     NULL,&dwType,
-                     (LPBYTE)&dwCpuUsage,
-                     &dwDataSize );
-
+		"KERNEL\\CPUUsage",
+		NULL,&dwType,
+		(LPBYTE)&dwCpuUsage,
+		&dwDataSize );
+	
     RegCloseKey(hkey);
     
     // geting current counter's value
     if ( RegOpenKeyEx( HKEY_DYN_DATA,
-                       "PerfStats\\StatData",
-                       0,KEY_READ,
-                       &hkey ) != ERROR_SUCCESS)
-       return 0;
+		"PerfStats\\StatData",
+		0,KEY_READ,
+		&hkey ) != ERROR_SUCCESS) {
+		m_usage = 0;
+		return;
+	}
+	
+	dwDataSize=sizeof(DWORD);
+	RegQueryValueEx( hkey,
+		"KERNEL\\CPUUsage",
+		NULL,&dwType,
+		(LPBYTE)&dwCpuUsage,
+		&dwDataSize );
 
-        dwDataSize=sizeof(DWORD);
-        RegQueryValueEx( hkey,
-                         "KERNEL\\CPUUsage",
-                         NULL,&dwType,
-                         (LPBYTE)&dwCpuUsage,
-                         &dwDataSize );
-        Sleep(1000);
-        RegQueryValueEx( hkey,
-                         "KERNEL\\CPUUsage",
-                         NULL,&dwType,
-                         (LPBYTE)&dwCpuUsage,
-                         &dwDataSize );
-        usage = (UINT)dwCpuUsage;
-
+	WaitForSingleObject(m_h_event,1000);
+	RegQueryValueEx( hkey,
+		"KERNEL\\CPUUsage",
+		NULL,&dwType,
+		(LPBYTE)&dwCpuUsage,
+		&dwDataSize );
+	usage = (UINT)dwCpuUsage;
+	
     RegCloseKey(hkey);
     
     // stoping the counter
     if ( RegOpenKeyEx( HKEY_DYN_DATA,
-                       "PerfStats\\StopStat",
-                       0,KEY_ALL_ACCESS,
-                       &hkey ) != ERROR_SUCCESS)
-       return 0;
-
-    dwDataSize=sizeof(DWORD);
-    RegQueryValueEx( hkey,
-                     "KERNEL\\CPUUsage",
-                     NULL,&dwType,
-                     (LPBYTE)&dwCpuUsage,
-                     &dwDataSize );
-
-    RegCloseKey(hkey);
-
-    return usage;
-}
-
-static bool g_is_osvi_got = false;
-static OSVERSIONINFO g_osvi;
-
-UINT CPUUsage() {
-	if ( ! g_is_osvi_got ) {
-		g_is_osvi_got = true;
-
-		ZeroMemory(&g_osvi, sizeof(OSVERSIONINFO));
-		g_osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
-		GetVersionEx (&g_osvi);
+		"PerfStats\\StopStat",
+		0,KEY_ALL_ACCESS,
+		&hkey ) != ERROR_SUCCESS) {
+		m_usage = 0;
+		return;
 	}
 	
-	if(g_osvi.dwPlatformId == 1) { // Win 9x/ME
-		return CPUUsage9X();
-	} else if(g_osvi.dwPlatformId == 2) { // Win NT
-		return CPUUsageNT();
-	} else {
-		return 0;
+    dwDataSize=sizeof(DWORD);
+    RegQueryValueEx( hkey,
+		"KERNEL\\CPUUsage",
+		NULL,&dwType,
+		(LPBYTE)&dwCpuUsage,
+		&dwDataSize );
+	
+    RegCloseKey(hkey);
+}
+
+void CPUUsage::CPUUsageThread()
+{
+	if ( ! m_is_osvi_got ) {
+		m_is_osvi_got = true;
+		
+		ZeroMemory(&m_osvi, sizeof(OSVERSIONINFO));
+		m_osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+		GetVersionEx (&m_osvi);
+	}
+	
+	if(m_osvi.dwPlatformId == 1) { // Win 9x/ME
+		CPUUsageThread9X();
+	}
+	else if(m_osvi.dwPlatformId == 2) { // Win NT
+		CPUUsageThreadNT();
 	}
 }
 
+CPUUsage::CPUUsage(void) : m_is_osvi_got(false) , m_usage(0) , m_thread_exit(0)
+{
+	m_h_event = ::CreateEvent(NULL,TRUE,FALSE,NULL);
+}
+
+CPUUsage::~CPUUsage(void)
+{
+	::CloseHandle(m_h_event);
+}
+
+void CPUUsage::Load(void)
+{
+}
+
+void CPUUsage::Unload(void)
+{
+	::SetEvent(m_h_event);
+}
--- trunk/impl_saori/ccpuid/cpuusage.h
+++ trunk/impl_saori/ccpuid/cpuusage.h
@@ -4,65 +4,26 @@
 #define __CPUUSAGE_H
 #include <windows.h>
 
-#define SystemBasicInformation       0
-#define SystemPerformanceInformation 2
-#define SystemTimeInformation        3
+class CPUUsage {
+private:
+	bool m_is_osvi_got;
+	OSVERSIONINFO m_osvi;
+	UINT m_usage;
 
-#define Li2Double(x) ((double)((x).HighPart) * 4.294967296E9 + (double)((x).LowPart))
+	volatile int m_thread_exit;
+	HANDLE m_h_event;
 
-typedef struct
-{
-    DWORD   dwUnknown1;
-    ULONG   uKeMaximumIncrement;
-    ULONG   uPageSize;
-    ULONG   uMmNumberOfPhysicalPages;
-    ULONG   uMmLowestPhysicalPage;
-    ULONG   uMmHighestPhysicalPage;
-    ULONG   uAllocationGranularity;
-    PVOID   pLowestUserAddress;
-    PVOID   pMmHighestUserAddress;
-    ULONG   uKeActiveProcessors;
-    BYTE    bKeNumberProcessors;
-    BYTE    bUnknown2;
-    WORD    wUnknown3;
-} SYSTEM_BASIC_INFORMATION;
+	void CPUUsageThread9X();
+	void CPUUsageThreadNT();
 
-typedef struct
-{
-    LARGE_INTEGER   liIdleTime;
-    DWORD           dwSpare[76];
-} SYSTEM_PERFORMANCE_INFORMATION;
+public:
+	CPUUsage(void);
+	~CPUUsage(void);
 
-typedef struct
-{
-    LARGE_INTEGER liKeBootTime;
-    LARGE_INTEGER liKeSystemTime;
-    LARGE_INTEGER liExpTimeZoneBias;
-    ULONG         uCurrentTimeZoneId;
-    DWORD         dwReserved;
-} SYSTEM_TIME_INFORMATION;
+	void Load(void);
+	void Unload(void);
 
-
-// ntdll!NtQuerySystemInformation (NT specific!)
-//
-// The function copies the system information of the
-// specified type into a buffer
-//
-// NTSYSAPI
-// NTSTATUS
-// NTAPI
-// NtQuerySystemInformation(
-//    IN UINT SystemInformationClass,    // information type
-//    OUT PVOID SystemInformation,       // pointer to buffer
-//    IN ULONG SystemInformationLength,  // buffer size in bytes
-//    OUT PULONG ReturnLength OPTIONAL   // pointer to a 32-bit
-//                                       // variable that receives
-//                                       // the number of bytes
-//                                       // written to the buffer
-// );
-typedef LONG (WINAPI *PROCNTQSI)(UINT,PVOID,ULONG,PULONG);
-
-
-UINT CPUUsage();
+	void CPUUsageThread();
+};
 
 #endif /* __CPUUSAGE_H */
