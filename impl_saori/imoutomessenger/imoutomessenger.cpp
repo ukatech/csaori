10f58d2b3cf1a08de5973361a1e62700ba5a00aa
ponapalt <ponapalt@2c982c0d-f248-0410-a771-998648ebd3bd>
2012-04-30 22:25:51 +0000

    半分ぐらい実装

--- impl_saori/imoutomessenger/imoutomessenger.cpp
+++ impl_saori/imoutomessenger/imoutomessenger.cpp
@@ -22,23 +22,32 @@ bool CSAORI::unload(){
 }
 
 /*---------------------------------------------------------
-	é©ìÆfree
----------------------------------------------------------*/
-class CAutoFree {
-private:
-	CAutoFree() { }
-
-	void *m_pBuf;
-public:
-	CAutoFree(void* pBuf) : m_pBuf(pBuf) { }
-	~CAutoFree() { if ( m_pBuf ) { free(m_pBuf); } }
-
-	operator void*() { return m_pBuf; }
-};
-
-/*---------------------------------------------------------
 	é¿çs
 ---------------------------------------------------------*/
+class ILWindowsDef {
+public:
+	ILWindowsDef(HWND h,const char *t) : hwnd(h), title(t) { }
+
+	HWND hwnd;
+	std::string title;
+};
+
+typedef std::vector<ILWindowsDef> ILWindowsList;
+
+static BOOL CALLBACK EnumILWindowsProc(HWND hwnd,LPARAM lParam)
+{
+	char buf[512];
+	::GetWindowText(hwnd,buf,sizeof(buf)-1);
+
+	if ( strncmp(buf,"IMOUTOMAIN_",11) == 0 ) {
+		ILWindowsList *pList = reinterpret_cast<ILWindowsList*>(lParam);
+
+		pList->push_back(ILWindowsDef(hwnd,buf+11));
+	}
+
+	return TRUE;
+}
+
 void CSAORI::exec(const CSAORIInput& in,CSAORIOutput& out){
 	out.result_code = SAORIRESULT_BAD_REQUEST;
 	if ( in.args.size() < 1 ) { return; }
@@ -46,22 +55,60 @@ void CSAORI::exec(const CSAORIInput& in,CSAORIOutput& out){
 	DWORD flag = SND_ASYNC;
 	std::string filepath;
 
-	if ( wcsnicmp(in.args[0].c_str(),L"system:",7) == 0 ) {
-		flag |= SND_ALIAS_ID;
-		filepath = SAORI_FUNC::UnicodeToMultiByte(in.args[0].c_str()+7);
+	if ( wcsnicmp(in.args[0].c_str(),L"list",4) == 0 ) {
+		ILWindowsList list;
+		::EnumWindows(EnumILWindowsProc,reinterpret_cast<LPARAM>(&list));
+
+		if ( list.size() == 0 ) {
+			out.result_code = SAORIRESULT_NO_CONTENT;
+			return;
+		}
+
+		out.result_code = SAORIRESULT_OK;
+
+		string_t txt;
+
+		size_t n = list.size();
+		string_t title;
+
+		for ( size_t i = 0 ; i < n ; ++i ) {
+			title = SAORI_FUNC::MultiByteToUnicode(list[i].title.c_str());
+			out.values.push_back(title);
+			txt += title;
+			txt += L"\1";
+		}
+
+		if ( txt.size() ) {
+			txt.erase(txt.size()-1,1);
+		}
+		out.result = txt;
 	}
-	else {
-		flag |= SND_NODEFAULT | SND_FILENAME;
-		filepath = checkAndModifyPath(SAORI_FUNC::UnicodeToMultiByte(in.args[0]));
+	else if ( wcsnicmp(in.args[0].c_str(),L"send",4) == 0 ) {
+		ILWindowsList list;
+		::EnumWindows(EnumILWindowsProc,reinterpret_cast<LPARAM>(&list));
+
+		if ( list.size() == 0 ) {
+			out.result_code = SAORIRESULT_NO_CONTENT;
+			return;
+		}
+
+		HWND hWndToSend = NULL;
+		if ( in.args.size() >= 2 ) {
+			size_t n = list.size();
+			std::string find_str = SAORI_FUNC::UnicodeToMultiByte(in.args[1]);
+
+			for ( size_t i = 0 ; i < n ; ++i ) {
+				if ( strstr(list[i].title.c_str(),find_str.c_str()) ) {
+					hWndToSend = list[i].hwnd;
+					break;
+				}
+			}
+		}
+		if ( hWndToSend == NULL ) {
+			hWndToSend = list[0].hwnd;
+		}
+
+		out.result_code = SAORIRESULT_OK;
 	}
-
-	out.result_code = SAORIRESULT_OK;
-
-	BOOL result = ::PlaySound(filepath.c_str(),NULL,flag);
-
-	char_t buf[32];
-	swprintf(buf,L"%d",result);
-
-	out.result = buf;
 }
 
